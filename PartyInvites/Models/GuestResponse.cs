2412755bc527e7ae7cea38bd0b2d3a50217ef22c
Tim Sellmyer <tsellmyer@talonlpe.com>
2018-02-03 13:30:07 -0600

    Guest Response model update

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -10,14 +10,17 @@ namespace PartyInvites.Models
     {
         [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
+
         [Required(ErrorMessage = "Please enter your email address")]
         [RegularExpression(".+\\@.+\\..+",
             ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
+
         [Required(ErrorMessage = "Please enter your phone number")]
         public string Phone { get; set; }
+
         [Required(ErrorMessage = "Please specify whether you'll attend")]
-        public bool WillAttend { get; set; }
+        public bool? WillAttend { get; set; }
 
     }
 }
